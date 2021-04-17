81c662e30d5ed8d4ff9b292884cfd2d0defc9ca4
CaduSi <carlos.eduardo@bytebio.com>
2021-04-17 13:22:29 -0300

    add menu responsivo

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-// const menuMobile = document.querySelector(".nav-mobile");
-// const menuDesktop = document.querySelector(".nav-desk");
+const menuMobile = document.querySelector(".menu-mobile");
+const menuDesktop = document.querySelector(".menu-desk");
 
-// if (window.innerWidth < 680) {
-//   console.log("Mostra Menu Mobile");
-//   menuMobile.classList.remove("none");
-//   menuDesktop.classList.add("none");
-//   // }
-// } else if (window.innerWidth > 680) {
-//   console.log("Mostra Menu desk");
-//   menuMobile.classList.add("none");
-//   menuDesktop.classList.remove("none");
-// }
+if (window.innerWidth < 680) {
+  console.log("Mostra Menu Mobile");
+  menuMobile.classList.remove("none");
+  menuDesktop.classList.add("none");
+  // }
+} else if (window.innerWidth > 680) {
+  console.log("Mostra Menu desk");
+  menuMobile.classList.add("none");
+  menuDesktop.classList.remove("none");
+}
 
-// window.addEventListener("resize", () => {
-//   // console.log(window.innerWidth);
+window.addEventListener("resize", () => {
+  // console.log(window.innerWidth);
 
-//   if (window.innerWidth < 680) {
-//     console.log("Mostra Menu Mobile");
-//     menuMobile.classList.remove("none");
-//     menuDesktop.classList.add("none");
-//   } else if (window.innerWidth > 680) {
-//     console.log("Mostra Menu desk");
-//     menuMobile.classList.add("none");
-//     menuDesktop.classList.remove("none");
-//   }
-// });
+  if (window.innerWidth < 680) {
+    console.log("Mostra Menu Mobile");
+    menuMobile.classList.remove("none");
+    menuDesktop.classList.add("none");
+  } else if (window.innerWidth > 680) {
+    console.log("Mostra Menu desk");
+    menuMobile.classList.add("none");
+    menuDesktop.classList.remove("none");
+  }
+});
